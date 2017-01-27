badb189846ad49abfc0529a90c7e9713661b0df2
Ahmet Goker <ahmetgoker02@gmail.com>
2017-01-27 09:31:12 +0100

    Server en application aangemaakt
    Aangemaakt van server het werkt en gemaakt van application url werkt

---
+++ src/ahmet/ap/edu/MedicijnApplication.java
@@ -0,0 +1,24 @@
+package ahmet.ap.edu;
+
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+
+
+public class MedicijnApplication extends Application {
+	@Override
+	public synchronized Restlet createInboundRoot(){
+		
+		Router router = new Router(getContext());
+
+        // Defines only one route
+        router.attach("/medicijn", MedicijnResource.class);
+
+        return router;
+		
+		
+		
+	}
+
+}
---
+++ src/ahmet/ap/edu/MedicijnServer.java
@@ -0,0 +1,29 @@
+package ahmet.ap.edu;
+
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+
+public class MedicijnServer {
+
+	public static void main(String[] args) {
+try {
+			
+			// Create a new Component.
+		    Component component = new Component();
+	
+		    // Add a new HTTP server listening on port 8181.
+		    component.getServers().add(Protocol.HTTP, 8186);
+	
+		    // Attach the sample application.
+		    component.getDefaultHost().attach("/medicijn", new MedicijnApplication());
+			component.start();
+		} 
+	    catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	
+
+}
